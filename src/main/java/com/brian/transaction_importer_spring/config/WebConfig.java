720797365fa373002a2c3cedb504e17ef0e51223
Brian Kelly <wolfyman06@gmail.com>
2026-02-16 21:59:41 -0700

    pass ui host origin in from properties and env

--- src/main/java/com/brian/transaction_importer_spring/config/WebConfig.java
+++ src/main/java/com/brian/transaction_importer_spring/config/WebConfig.java
@@ -1,16 +1,22 @@
 package com.brian.transaction_importer_spring.config;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Data
 @Configuration
+@ConfigurationProperties(prefix = "web")
 public class WebConfig implements WebMvcConfigurer {
+
+    private String uiHostOrigin;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Apply to all endpoints
-                .allowedOrigins("http://localhost:3000", System.getenv("UI_HOST_ORIGIN"))  // Your React app
+                .allowedOrigins("http://localhost:3000", uiHostOrigin)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
